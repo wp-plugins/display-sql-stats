1a2e78b5d00309473a25e91968266db991026472
1manfactory <1manfactory@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-09-02 07:44:35 +0000

    initial upload of old files
    git-svn-id: https://plugins.svn.wordpress.org/display-sql-stats/tags/0.9.1@1236088 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ display-stats-header.php
@@ -0,0 +1,10 @@
+<!-- inserted by Wordpress plugin Display SQL Stats start-->
+<!--Load the AJAX API-->
+<script type="text/javascript" src="https://www.google.com/jsapi"></script>
+<script type="text/javascript">
+
+  // Load the Visualization API and the piechart package.
+  google.load('visualization', '1.0', {'packages':['corechart']});
+
+</script>
+<!-- inserted by Wordpress plugin Display SQL Stats end-->
---
+++ display-stats-setchart-single.php
@@ -0,0 +1,40 @@
+<?php
+function dss_setchart($dssno) {
+	global $chart_types_array;
+	// get SQL statement data to be used
+	
+	$dss_switch_array=get_option("dss_switch_array");
+	$dss_title_array=get_option("dss_title_array");
+	$dss_type_array=get_option("dss_type_array");
+	
+	$dss_switch=$dss_switch_array[$dssno];
+	$dss_title=$dss_title_array[$dssno];
+	$dss_type=$dss_type_array[$dssno];	
+	//print_r ($dss_type_array_to_use);
+	$returnvalue="";
+	
+		
+		// chart visible?
+		if ($dss_switch=="on") {
+			$returnvalue.= '	
+			var options'.$dssno.' = {\'title\':\''.$dss_title.'\',
+				\'is3D\':true,
+				\'width\':500,
+				\'height\':500';
+				// only set min/max values with barcharts
+				if ($chart_types_array[$dss_type]=="BarChart") $returnvalue.= ',
+				\'hAxis\': { maxValue: \''.$maxval[$i].'\', minValue: \''.$minval[$i].'\', format: \'0\'}
+				';
+				
+				$returnvalue.= '};
+
+			
+			var chart'.$dssno.' = new google.visualization.'.$chart_types_array[$dss_type].'(document.getElementById(\'chart_div'.$dssno.'\'));
+			chart'.$dssno.'.draw(data'.$dssno.', options'.$dssno.');
+			
+			';
+		}
+	
+	return $returnvalue;
+}
+?>
